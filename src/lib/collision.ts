f75e3992fb59660979f635ff7acc1246ac8274b7
cheesepencil <cheesepencil@noreply>
2023-09-11 22:57:12 -0400

    platformer con't

--- src/lib/collision.ts
+++ src/lib/collision.ts
@@ -32,17 +32,43 @@ class Collision {
         return true
     }
 
-    static collideSide(sprite: { x: number, w: number, dx: number, y: number, flags: number[] }) {
-        const offset = Math.floor(sprite.w / 3)
+    static collideSideMap(sprite: Sprite, flag: number) {
+        const offset = sprite.w / 3
 
-        for (let i = -(offset); i += offset; i == 2) {
-            // if tile at sprite.x + offset / sprite.w, sprite.y + i / sprite.h 
-            // has a flag in flags then...
-            if (true) {
+        for (let i = -offset; i += 2; i <= offset) {
+            let tile = mget((sprite.x + offset) / 8, (sprite.y + i) / 8)
+            if (fget(tile, flag)) {
                 sprite.dx = 0
                 sprite.x = (Math.floor(((sprite.x + (offset)) / sprite.w)) * sprite.w) - (offset)
                 return true
             }
+            tile = mget((sprite.x - offset) / 8, (sprite.y + i) / 8)
+            if (fget(tile, flag)) {
+                sprite.dx = 0
+                sprite.x = (Math.floor((sprite.x - offset) / 8) * 8) + 8 + offset
+                return true
+            }
         }
+
+        return false
+    }
+
+    static collideFloorMap(sprite: Sprite, flag: number) {
+        if (sprite.dy < 0) return false
+
+        const offset = sprite.w / 3
+        let landed = false
+        for (let i = -offset; i += 2; i <= offset) {
+            let tile = mget((sprite.x + i) / 8, (sprite.y + (sprite.h / 2)) / 8)
+            if (fget(tile, flag)) {
+                sprite.dy = 0
+                sprite.y = (Math.floor((sprite.y + (sprite.h / 2)) / 8) * 8) - (sprite.h / 2)
+                sprite.grounded = true
+                sprite.airTime = 0
+                landed = true
+            }
+        }
+
+        return landed
     }
 }
--- src/lib/sprite.ts
+++ src/lib/sprite.ts
@@ -1,6 +1,8 @@
 interface SpriteConfig {
     x?: number
     y?: number
+    dx?: number
+    dy?: number
     widthSprites?: number
     heightSprites?: number
     index: number
@@ -10,16 +12,28 @@ interface SpriteConfig {
 class Sprite implements Positionable {
     x: number
     y: number
+    dx: number
+    dy: number
+    w: number
+    h: number
     widthSprites: number
     heightSprites: number
+    grounded: boolean
+    airTime: number
     index: number
     colorKey: number | number[]
 
     constructor(config: SpriteConfig) {
         this.x = config.x || 0
         this.y = config.y || 0
+        this.dx = config.dx || 0
+        this.dy = config.dy || 0
         this.widthSprites = config.widthSprites || 1
+        this.w = this.widthSprites * 8
         this.heightSprites = config.heightSprites || 1
+        this.h = this.heightSprites * 8
+        this.grounded = false
+        this.airTime = 0
         this.index = config.index
         this.colorKey = config.colorKey
     }
